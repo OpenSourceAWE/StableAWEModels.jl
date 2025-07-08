b303aa36f4c3f06ee2eb6617d21fda2a3bcad5a0
1-Bart-1 <bart@vandelint.net>
2025-07-08 21:08:57 +0200

    Increase abstol

--- test/test_ram_air_kite.jl
+++ test/test_ram_air_kite.jl
@@ -29,8 +29,8 @@ const BUILD_SYS = true
     @info "Creating s:"
     @time s = SymbolicAWEModel(set)
 
-    s.set.abs_tol = 1e-3
-    s.set.rel_tol = 1e-3
+    s.set.abs_tol = 5e-2
+    s.set.rel_tol = 1e-2
 
     # Initialize at elevation
     set.elevation = 80.0
