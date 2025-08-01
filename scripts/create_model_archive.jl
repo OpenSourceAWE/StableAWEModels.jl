07856ba969b3b4488ccd2c6062296998eb601899
1-Bart-1 <bart@vandelint.net>
2025-08-01 22:56:03 +0200

    Use current default manifest

--- scripts/create_model_archive.jl
+++ scripts/create_model_archive.jl
@@ -1,6 +1,11 @@
 # Copyright (c) 2024, 2025 Bart van de Lint, Uwe Fechner
 # SPDX-License-Identifier: MIT
 
+version = VERSION.minor
+cp("Manifest-v1.$version.toml.default", "Manifest-v1.$version.toml"; force=true)
+using Pkg
+Pkg.instantiate()
+
 using Timers
 @info "Loading packages"
 tic()
@@ -13,7 +18,6 @@ models = SymbolicAWEModels.create_default_models(; prn)
 @info "Created all models"
 toc()
 
-version = VERSION.minor
 SymbolicAWEModels.create_model_archive(get_data_path(), 
                                        joinpath(get_data_path(), "models_v1.$version.tar.gz"))
 
