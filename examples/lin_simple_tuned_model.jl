96119774aae53128dd69f94151e7e1d0372634a3
1-Bart-1 <bart@vandelint.net>
2025-08-03 15:03:54 +0200

    Some kind of working example

---
+++ examples/lin_simple_tuned_model.jl
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2025 Bart van de Lint <bart@vandelint.net>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+using Pkg
+if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
+    using TestEnv; TestEnv.activate()
+end
+
+using SymbolicAWEModels, VortexStepMethod, KiteUtils, ControlPlots, Statistics
+using OrdinaryDiffEqCore
+
+set = Settings("system.yaml")
+sam = SymbolicAWEModel(set, "ram")
+init!(sam)
+
+tether_set = Settings("system.yaml")
+tether_sam = SymbolicAWEModel(tether_set, "tether")
+init!(tether_sam)
+
+simple_set = Settings("system.yaml")
+simple_sam = SymbolicAWEModel(simple_set, "simple_ram")
+init!(simple_sam)
+
+sim_oscillate!(sam; total_time=1.0)
+SymbolicAWEModels.copy_to_simple!(sam, tether_sam, simple_sam)
+
+bias = 0.2
+# sl = sim_oscillate!(sam; total_time=5.0, prn=true, bias) # TODO: add first frac ram model
+# display(plot(sam.sys_struct, sl))
+
+lin_model = linearize!(simple_sam)
+sl, lin_sl = sim_oscillate!(simple_sam; total_time=0.1, prn=true, bias, lin_sim=lin_model)
+display(plot(simple_sam.sys_struct, sl))
+
